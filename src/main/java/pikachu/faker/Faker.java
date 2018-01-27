ef42d784e9dc0ca50512c89972cd6b9ff98981b5
Frederick-S <mao_xiaodan@hotmail.com>
2018-01-27 22:56:28 +0800

    Init

---
+++ src/main/java/pikachu/faker/Faker.java
@@ -0,0 +1,11 @@
+package pikachu.faker;
+
+public class Faker<T> {
+    public Faker field(String fieldName) {
+        return this;
+    }
+
+    public T generate() {
+        return null;
+    }
+}
